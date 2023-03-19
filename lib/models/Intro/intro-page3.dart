4fc536870faa10a9de284beeaf1d25bff692c909
asiabadnjki <assiabadnjki@gmail.com>
2023-03-19 18:24:57 +0200

    update

---
+++ lib/models/Intro/intro-page3.dart
@@ -0,0 +1,69 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+class Intro3 extends StatelessWidget { 
+  @override
+  Widget build(BuildContext Context) {
+ return Center(
+   child: Column(
+     children: [
+        //  Lottie.network(
+        //         'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
+        //         height: 200),
+       Padding(
+         padding: const EdgeInsets.fromLTRB(8, 90, 8, 8),
+         child: Container(
+          height: 300,
+          color: Colors.white,
+          child: 
+            Image.asset('assets/images/8.png')
+          
+         ),
+       ),
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey),
+                    fixedSize:
+                        MaterialStateProperty.all(const Size.fromWidth(300))),
+                onPressed: () {
+                 // Get.to(SignIn());
+                 },
+                child: Text(
+                  "Login".tr,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+         ),
+     Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey),
+                    fixedSize:
+                        MaterialStateProperty.all(const Size.fromWidth(300),
+                        )),
+                onPressed: () {
+                 },
+                child: Text(
+                  "Sign Up".tr,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+         ),
+     ],
+   ),
+ );
+
+  }}
---
+++ lib/models/Intro/intro.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:graduationproject/models/Intro/intro-controller.dart';
+import 'package:graduationproject/models/timer/controller/timer_controller.dart';
+import 'intro-page1.dart';
+import 'intro-page2.dart';
+import 'intro-page3.dart';
+
+class IntroPageView extends GetResponsiveView<IntroController> { 
+PageController controllers= PageController();
+bool onlastpage= false;
+  @override
+  Widget build(BuildContext Context) {
+  return Scaffold(
+     body: Stack(children: [
+      PageView( controller: controllers,
+      onPageChanged: (index) {
+        onlastpage= (index == 2);
+      },
+        children: [
+      Intro1(),
+      Intro2(),
+      Intro3()
+         
+     ],),
+    Container(
+      alignment: Alignment(0,0.75),
+      child:
+       Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         children: [
+          GestureDetector(
+            onTap: (){
+             // controllers.jumpToPage(1);
+              controllers.previousPage(duration: Duration(microseconds: 500), curve: Curves.ease);
+            },
+            child:  Padding(
+              padding: const EdgeInsets.fromLTRB(20, 8, 8, 8),
+              child: Text("skip",style: TextStyle(color: Colors.black54,fontSize: 18),
+              ),
+            ),),
+            
+          // SmoothPageIndicator(controller: controllers, count: 3),
+           onlastpage?
+            GestureDetector(
+            onTap: (){
+         //     go to main
+            },
+            child:      Text('Done') ,
+            ):  GestureDetector(
+            onTap: (){
+              controllers.nextPage(duration: Duration(microseconds: 500), curve: Curves.easeIn);
+            },
+            child:      Padding(
+              padding: const EdgeInsets.fromLTRB(8, 8, 20, 8),
+              child: Text('next',style: TextStyle(color: Colors.black54,fontSize: 18),),
+            ) ,
+            )
+         ],
+       ))
+     ],)
+     
+  ) ;
+
+  }}
