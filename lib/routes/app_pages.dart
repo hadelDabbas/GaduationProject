fb17a702017f001bcc9813b03702bf9d5e9bea85
asiabadnjki <assiabadnjki@gmail.com>
2023-03-19 18:24:39 +0200

    routes

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+import '../models/Intro/intro.dart';
+
+class AppPages {
+  AppPages._();
+
+  // static final routes = [
+  //   GetPage(
+  // name:Intro ,
+  //     page: () => IntroPageView (),
+  //   ),];
+}
