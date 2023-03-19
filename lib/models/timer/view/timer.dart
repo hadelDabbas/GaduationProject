ce385556f184eac1c4bdc5311c004c238634cf6e
asiabadnjki <assiabadnjki@gmail.com>
2023-03-19 17:25:02 +0200

    push-photo

---
+++ lib/models/timer/view/timer.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'package:graduationproject/models/timer/controller/timer_controller.dart';
+
+class TimerPageView extends GetResponsiveView<Timercontroller>{
+Timercontroller controller=Get.put(Timercontroller());
+
+}
